0972f745ed1700e537ad316ed4ac24f3413e17fc
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-01 20:58:38 -0300

    fix

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-const minhaVariavel = process.env.MINHA_VARIAVEL; // Substitua MINHA_VARIAVEL pelo nome da sua variável
-console.log(`Valor da Variável de Ambiente: ${minhaVariavel}`);
+console.log(`oi`);
