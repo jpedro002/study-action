19b2e32e188ec5c6bb868a54b6d0bde9de4a4ceb
João Pedro <122836400+jpedro002@users.noreply.github.com>
2023-10-09 09:33:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(''helloo)
+ const github = require('@actions/github');
+          const context = github.context;
+
+          if (context.eventName === 'push' && context.payload.pull_request) {
+            console.log('Este evento é o resultado da mesclagem de um pull request.');
+          } else {
+            console.log('Este evento é um push regular.');
+          }
