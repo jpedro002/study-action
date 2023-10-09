3e9531b7267bf25e6d2912044f7ef90e0ab0172c
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-09 11:02:51 -0300

    fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const github = require("@actions/github");
-const { context } = require("@actions/github");
 
 const context = github.context;
+
 
 if (context.eventName === "push" && context.payload.pull_request) {
   console.log("Este evento é o resultado da mesclagem de um pull request.");
@@ -9,6 +9,4 @@ if (context.eventName === "push" && context.payload.pull_request) {
   console.log("Este evento é um push regular.");
 }
 
-
-const eventName = context.eventName;
-console.log(`O nome do evento é: ${eventName}`);
+console.log(`O nome do evento é: ${context.eventName}`);
