32fbc4d09c84c71c1bda40e6734b205c64aa299d
João Pedro <122836400+jpedro002@users.noreply.github.com>
2023-10-09 10:59:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
- const github = require('@actions/github');
-          const context = github.context;
+const github = require("@actions/github");
+const { context } = require("@actions/github");
 
-          if (context.eventName === 'push' && context.payload.pull_request) {
-            console.log('Este evento é o resultado da mesclagem de um pull request.');
-          } else {
-            console.log('Este evento é um push regular.');
-          }
+const context = github.context;
+
+if (context.eventName === "push" && context.payload.pull_request) {
+  console.log("Este evento é o resultado da mesclagem de um pull request.");
+} else {
+  console.log("Este evento é um push regular.");
+}
+
+
+const eventName = context.eventName;
+console.log(`O nome do evento é: ${eventName}`);
