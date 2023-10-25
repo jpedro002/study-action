5a610f2c8546e903f0913d222c32eb27f1511475
jpedro002 <joaopsilvavolei@gmail.com>
2023-10-25 14:03:04 -0300

    log is merge ou n

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const github = require("@actions/github");
 const context = github.context;
 
 
+
 if (context.eventName === "push" && context.payload.pull_request) {
   console.log("Este evento é o resultado da mesclagem de um pull request.");
 } else {
   console.log("Este evento é um push regular.");
 }
-
+console.log(context.payload.pull_request.merged);
 
 
 console.log(`O nome do evento é: ${context.payload.pull_request}`);
